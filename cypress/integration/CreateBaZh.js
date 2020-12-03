a3c8871f84deff87e2731897e59b937215cc6aa4
Marcin Pogorzelski <mpogorzelski@infoprojekt.pl>
2020-12-03 14:28:40 +0100

    next commits

--- cypress/integration/CreateBaZh.js
+++ cypress/integration/CreateBaZh.js
@@ -1,6 +1,10 @@
 describe('CreateBa', () => {
     it('Create BA ZH', () => {
-        cy.visit("https://10-tim.imfint.local");
-        expect(true).to.equal(true);
-    });
-});
+        cy.visit("https://10-tim.imfint.local")
+        cy.contains('Log in').click()
+        cy.url().should('include','login')
+        cy.contains('username')
+        
+        //expect(true).to.equal(true)
+    })
+})

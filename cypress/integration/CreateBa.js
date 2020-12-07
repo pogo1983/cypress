8f0d1b46a8a7166a8d6dfa0719f63ddfa230f84d
Marcin Pogorzelski <mpogorzelski@infoprojekt.pl>
2020-12-07 11:28:14 +0100

    Create the contract

--- cypress/integration/CreateBa.js
+++ cypress/integration/CreateBa.js
@@ -39,7 +39,32 @@ describe('CreateBa', () => {
         cy.get('div.col-xs-8:nth-child(1) > div:nth-child(1) > div:nth-child(1) > form:nth-child(1) > fieldset:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > p:nth-child(1)').should('contain','ZH')
         //create activation
         cy.get('li.ng-isolate-scope:nth-child(5) > a:nth-child(1)').click()
-
+        cy.get('div.ng-grid-column-action-alignment:nth-child(1) > a:nth-child(2)').click()
+        cy.get('p.ng-binding:nth-child(2)').should('contain','ZH')
+        cy.get('div.btn-group:nth-child(3) > button:nth-child(1)').click()
+        //chose Uitgebreid
+        cy.get('div.btn-group:nth-child(3) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(3) > a:nth-child(1) > span:nth-child(1)').click()
+        cy.get('div.btn-group:nth-child(6) > button:nth-child(1)').click()
+        //chose 3rd party factoring
+        cy.get('div.btn-group:nth-child(6) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(3) > a:nth-child(1) > span:nth-child(1)').click()
+        cy.get('div.btn-group:nth-child(9) > button:nth-child(1)').click()
+        //chose IZ
+        cy.get('div.btn-group:nth-child(9) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(5) > a:nth-child(1) > span:nth-child(1)').click()
+        cy.get('div.btn-group:nth-child(12) > button:nth-child(1)').click()
+        //chose MZC
+        cy.get('div.btn-group:nth-child(12) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(3) > a:nth-child(1) > span:nth-child(1)').click()
+        cy.wait(6000)
+        //add prices
+        cy.get('div.ngRow:nth-child(2) > div:nth-child(7) > div:nth-child(2) > div:nth-child(1)').click().type('1')
+        cy.get('div.ngRow:nth-child(4) > div:nth-child(7) > div:nth-child(2) > div:nth-child(1)').click().type('1.2')
+        cy.get('div.ngRow:nth-child(8) > div:nth-child(7) > div:nth-child(2) > div:nth-child(1)').click().type('1.3')
+        cy.get('div.ngRow:nth-child(10) > div:nth-child(7) > div:nth-child(2) > div:nth-child(1)').click().type('1.4')
+        cy.wait(1000)
+        cy.get('button.pull-right:nth-child(1)').click()
+        //submitting parties
+        cy.get('.contract-participants-grid > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').should('contain','ZH')
+        //edit party
+        cy.get('div.col10:nth-child(1) > a:nth-child(1)').click()
         //cy.contains('Instituut').click()
         //.select('Instituut')
         //cy.contains('Infomedics TIM GUI').should('include','GUI')

086ab2667ea469d0e762edfb5645d86f85bdf937
Marcin Pogorzelski <mpogorzelski@infoprojekt.pl>
2021-06-28 10:59:49 +0200

    fix party type and expert team

--- cypress/integration/CreateBa.js
+++ cypress/integration/CreateBa.js
@@ -21,9 +21,13 @@ describe('CreateBa', () => {
         .type('POGO BA ZH TEST')
         cy.get(':nth-child(4) > :nth-child(1) > .col-xs-7 > .form-control')
         .type('NL35RABO0117713678')
-        cy.get('div.col-xs-6:nth-child(2) > div:nth-child(4) > div:nth-child(1) > div:nth-child(2)')
+        //choose type
+        cy.get('div.col-xs-6:nth-child(2) > div:nth-child(4) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > button:nth-child(1)')
         .click()
-        cy.get('ul.dropdown-menu:nth-child(1) > li:nth-child(3) > a:nth-child(1)').click()
+        cy.get('div.col-xs-6:nth-child(2) > div:nth-child(4) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > ul:nth-child(1)').contains('Instituut').click()
+        //choose Expert Team
+        cy.get('div.ng-invalid > button:nth-child(1)').click()
+        cy.get('div.ng-invalid > div:nth-child(2) > ul:nth-child(1)').contains('CHA 1').click()
         cy.get('#comment').type('POGO TEST BA ZH')
         //tab BA address
         cy.get('li.ng-isolate-scope:nth-child(2) > a:nth-child(1)').click()
@@ -33,7 +37,7 @@ describe('CreateBa', () => {
         cy.get(':nth-child(1) > :nth-child(3) > .col-xs-8 > .form-group > .col-xs-9 > .form-control').type('Gdansk')
         //tab Ba contact
         cy.get('[heading="Contact"] > .ng-binding').click()
-        cy.get(':nth-child(6) > :nth-child(1) > .col-xs-8 > .form-control').type('pogotest@pogo.pl')
+        cy.get('div.form-group:nth-child(8) > div:nth-child(1) > div:nth-child(2) > input:nth-child(1)').type('pogotest@pogo.pl')
         cy.get('[ng-click="saveOrUpdate($event)"]').click()
         cy.wait(1000)
         cy.get('div.col-xs-8:nth-child(1) > div:nth-child(1) > div:nth-child(1) > form:nth-child(1) > fieldset:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > p:nth-child(1)').should('contain','ZH')
